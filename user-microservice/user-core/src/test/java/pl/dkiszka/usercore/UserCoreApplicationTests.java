21d4a598d3288e0eb664618ff3b4b860c84f9312
dominikk19 <dominikk19@gmail.com>
2021-04-19 20:55:07 +0200

    initial commit

---
+++ user-microservice/user-core/src/test/java/pl/dkiszka/usercore/UserCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.dkiszka.usercore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserCoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
